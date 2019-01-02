95e7b96aaaadae2d9a42bde01a56dfba0c885eca
verrixkio <verrixkio@gmail.com>
2019-01-02 22:43:47 +0000

    Initial commit with all my work so far

---
+++ for-loop.js
@@ -0,0 +1,7 @@
+var total = 0
+var limit = 45
+
+for (var i = 0; i < limit; i++) {
+}
+total += i;
+console.log(total)
---
+++ rounding-numbers.js
@@ -0,0 +1,3 @@
+roundup = 1.5
+rounded = Math.round(roundup)
+console.log(rounded)
